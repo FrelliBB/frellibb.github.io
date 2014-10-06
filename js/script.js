7610f906c09c97c76925496400670f6785c4765b
= <=>
2014-10-06 12:17:22 +0200

    moved changecolour to title button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('#changeColour').click(function(e){
+			e.preventDefault();
 			var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
 			console.log('Switching colour to: ' + color);
 			$(document.body).animate({backgroundColor: color}, 'slow');
